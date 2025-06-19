8989e7495df61e924f17e861a8c1676cd2d62f47
Ivan24-wq <iv8747583@gmail.com>
2025-06-19 22:19:35 +0300

    Added settings menu

--- password/main.py
+++ password/main.py
@@ -7,6 +7,11 @@ from kivy.lang import Builder
 from kivy.core.window import Window
 from kivy.uix.popup import Popup
 from kivy.uix.label import Label
+from kivymd.app import MDApp
+from kivymd.uix.button import MDIconButton
+from kivymd.uix.boxlayout import MDBoxLayout
+from kivymd.uix.button import MDFlatButton
+    
 Window.clearcolor = (0.56, 0.93, 0.56, 1)
 Builder.load_file("password.kv")
 class Password(BoxLayout):
@@ -27,15 +32,30 @@ class Password(BoxLayout):
         except ValueError:
             password = 'Ошибка: введите корректное число'
         self.ids.result_label.text = password
-    def open_settings(self):
+    def open_settings(self, *args):
+        box = MDBoxLayout(orientation='vertical', spacing=10, padding=10)
+        label = Label(text="Настройки")
+        btn_change_theme = MDFlatButton(text="Сменить тему", on_release=lambda x: self.change_theme())
+        box.add_widget(label)
+        box.add_widget(btn_change_theme)
+
         popup = Popup(
             title = 'Настройки', 
-            content = Label(text = "Настройки"),
-            size_hint = (0.8, 0.4)
+            content = box,
+            size_hint=(0.8, 0.4)
         )
         popup.open()
-class MyApp(App):
+
+    def change_theme(self):
+        app = MDApp.get_running_app()
+        if app.theme_cls.theme_style == "Light":
+            app.theme_cls.theme_style = "Dark"
+        else:
+            app.theme_cls.theme_style = "Light"
+
+class MyApp(MDApp):
     def build(self):
+        self.theme_cls.theme_styles = "Light"
         return Password()
 if __name__ == "__main__":
     MyApp().run()
